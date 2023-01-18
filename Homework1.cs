0f534ba184ea358792ac94ce7ffc2f4c9ef09802
Caleb Withers <105404965+Lebyath@users.noreply.github.com>
2023-01-18 06:58:39 -0600

    Homework1 code file
    This is my first assignment for CIDM2315.

---
+++ Homework1.cs
@@ -0,0 +1,23 @@
+﻿/*The lineear line equation is: Z = 4X**2+3Y, 
+when X=2.5, Y=3.3, write a program to:
+    -calculate the value of Z and print it out.
+
+*/
+namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // my code for Homework1
+
+        //declare variables
+        double X = 2.5;
+        double Y = 3.3;
+
+        //write the equation
+        double Z = 4 * (X * X) + (3 * Y);
+
+        //print the answer
+        Console.WriteLine($"The value of Z is: {Z}");
+    }
+}
